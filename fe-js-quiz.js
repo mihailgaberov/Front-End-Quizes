d36c17554e55a2b5d5a5958be4128a50d3ef2cc2
Mihail Gaberov <mihail.gaberov@gmail.com>
2016-01-26 11:25:46 +0100

    Some more JS questions and their answers added.

--- fe-js-quiz.js
+++ fe-js-quiz.js
@@ -1,3 +1,44 @@
+var foo = function foo() {
+    console.log(foo === foo);  
+};
+foo();
+//What is printed in the console?
+
+//Answer: true
+=====================================
+function aaa() {
+    return
+    {
+        test: 1
+    };
+}
+alert(typeof aaa());
+What does the above alert?
+
+//Answer: undefined (because of the automatic adding of ";" after 'return' - closing the state, i.e. it's like return; { test: 1};)
+=====================================
+Number("1") - 1 == 0;
+
+// Answer: true, because the string "1" is being cast to number and then subtracts it
+=====================================
+(true + false) > 2 + true;
+
+// Answer: false, as true + false = 1 + 0 = 1 which is not bigger than 2 + true = 2 + 1 = 3 
+=====================================
+function bar() {
+    return foo;
+    foo = 10;
+    function foo() {}
+    var foo = '11';
+}
+alert(typeof bar());
+
+// Answer: function, because the type of bar is "function"
+=====================================
+"1" - - "1";
+
+// Answer: 2, as the minus signs convert the strings to numbers and actually do the calculation
+=====================================
 var x = 3;
 
 var foo = {
@@ -14,6 +55,7 @@ var go = foo.baz.bar;
 
 alert(go());	// 3
 alert(foo.baz.bar());	// 1
+// Because of the different scopes from where it's invoked
 =====================================
 new String("This is a string") instanceof String;	// true
 =====================================
@@ -66,5 +108,5 @@ function foo(a, b) {
 }
 foo(1); // undefined
 =====================================
-NaN === NaN;
+NaN === NaN;    // false as NaN is not equal even to itself
 =====================================
